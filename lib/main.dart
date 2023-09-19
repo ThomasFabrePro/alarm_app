c3f630fef594861963c6e1433008c5254bcb2c03
Thomas Fabre <thomas.fabre@outlook.fr>
2023-09-19 16:22:24 +0200

    Revert "notification received"
    This reverts commit 50c016dac46b277502b3def5e806483577a3ebe9.

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,18 @@
-import 'package:alarm_app/services/firebase_helper.dart';
 import 'package:alarm_app/services/notification_service.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
-  var message = await FirebaseMessaging.instance.getInitialMessage();
-  var data = message?.data;
-  print('🔵firebase messaging payload : ${message?.data}');
-  print('🔵firebase messaging payload : ${data}');
-  print("Handling a background message: ${message?.messageId}");
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   NotificationService notificationService = NotificationService();
   notificationService.initNotification();
 //!test
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
-  var token = await messaging.getToken();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  print('🔵User granted permission: ${settings.authorizationStatus}');
-  print('🔵firebasemessaging token : $token');
+
   notificationService.showNotification(
       title: 'TESTHelloWorld', body: 'TESTHelloWorld');
-  // await FirebaseHelper.sendNotification(
-  //     title: 'HelloWorld', body: 'HelloWorld', token: token!);
 //!end test
 
   // Set up the SettingsController, which will glue user settings to multiple
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,11 @@ import cloud_functions
 import firebase_auth
 import firebase_core
 import firebase_messaging
+<<<<<<< HEAD
 import firebase_storage
 import flutter_local_notifications
+=======
+>>>>>>> parent of 50c016d (notification received)
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -19,6 +22,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+<<<<<<< HEAD
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+=======
+>>>>>>> parent of 50c016d (notification received)
 }
