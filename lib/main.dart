caaec596907cb56cefbd8aa23afb5c7e60ec935e
Thomas Fabre <thomas.fabre@outlook.fr>
2023-10-25 15:09:22 +0200

    add notification permission request

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:alarm_app/services/notification_service.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 // ignore: depend_on_referenced_packages
 import 'package:timezone/data/latest.dart' as tz;
 import 'src/app.dart';
@@ -13,7 +14,10 @@ Logger logger = Logger(
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   NotificationService notificationService = NotificationService();
-
+  var permissionStatus = await Permission.notification.status;
+  if (permissionStatus.isDenied) {
+    await Permission.notification.request();
+  }
   tz.initializeTimeZones();
   notificationService.initNotification();
   runApp(const MyApp());
